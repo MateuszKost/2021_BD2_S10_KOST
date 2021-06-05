440886abb8f266778b99309700b09233be2cd189
MateuszKost <matekos1604@gmail.com>
2021-06-05 22:37:11 +0200

    Edit View

---
+++ SmartCollection/Client/Pages/Images/EditImage.razor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartCollection.Client.Pages.Images
+{
+    public partial class EditImage
+    {
+        string nName, nDescription, nTags;
+
+        public void Save(string nname, string ndescription, string ntags)
+        {
+            nName = nname;
+            nDescription = ndescription;
+            nTags = ntags;
+        }
+
+
+        TempImageModel temp = new TempImageModel() {Name = "nazwa", Description="opis", Tags="Tagi, Tagi2", URL= "/picture/monkey.jpg" };
+
+    }
+
+    public class TempImageModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Tags { get; set; }
+        public string URL { get; set; }
+    }
+}
