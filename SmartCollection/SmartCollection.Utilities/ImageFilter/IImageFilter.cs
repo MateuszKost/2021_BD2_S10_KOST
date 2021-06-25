69dfe81ec79d012adeb10eff2098af3e4ae21e8b
reoskaro434 <reoskaro434@gmail.com>
2021-06-25 19:44:57 +0200

    Create IImageFilter interface

---
+++ SmartCollection/SmartCollection.Utilities/ImageFilter/IImageFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCollection.Utilities.ImageFilter
+{
+    public interface IImageFilter<T> where T : class
+    {
+        public Task<T> FiltrAsync(string dateSortType,string tagName, string imageName); 
+    }
+}
---
+++ SmartCollection/SmartCollection.Utilities/ImageFilter/ImageFilter.cs
@@ -0,0 +1,17 @@
+﻿using SmartCollection.Models.ViewModels.ImagesViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCollection.Utilities.ImageFilter
+{
+    public class ImageFilter : IImageFilter<ImagesViewModel>
+    {
+        public Task<ImagesViewModel> FiltrAsync(string dateSortType, string tagName, string imageName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
