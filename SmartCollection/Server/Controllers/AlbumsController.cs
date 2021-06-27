9f876e94a87551368bb71b51182c75ccf725be9e
elkrzys <krzysztof.lipiec@outlook.com>
2021-06-27 03:05:01 +0200

    Added getting album cover - first version

--- SmartCollection/Server/Controllers/AlbumsController.cs
+++ SmartCollection/Server/Controllers/AlbumsController.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using SmartCollection.DataAccess.RepositoryPattern;
 using SmartCollection.Models.ViewModels.AlbumViewModel;
 using SmartCollection.Server.User;
+using SmartCollection.StorageManager.Containers;
+using SmartCollection.StorageManager.Context;
+using SmartCollection.Utilities.ImageConverter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,11 +23,18 @@ namespace SmartCollection.Server.Controllers
     {
         private readonly ICurrentUser _currentUser;
         private readonly IUnitOfWork _unitOfWork;
-
-        public AlbumsController(ICurrentUser currentUser, IUnitOfWork unitOfWork)
+        //TODO UTILITY
+        private readonly IStorageContext<IStorageContainer> _storageContext;
+        private readonly IImageConverter _imageConverter;
+        public AlbumsController(ICurrentUser currentUser, 
+            IUnitOfWork unitOfWork, 
+            IStorageContext<IStorageContainer> storageContext,
+            IImageConverter imageConverter)
         {
             _currentUser = currentUser;
             _unitOfWork = unitOfWork;
+            _storageContext = storageContext;
+            _imageConverter = imageConverter;
         }
 
         //[Authorize]
@@ -43,20 +53,28 @@ namespace SmartCollection.Server.Controllers
                 AlbumId = 0,
                 AlbumName = "All Images",
                 ImagesCount = _unitOfWork.Images.Find(image => image.UserId.Equals(userId)).ToList().Count,
-                AlbumCoverPicture = "no cover yet"
+                AlbumCoverPicture = null
             });
 
             if (albumList != null && albumList.Any())
             {
-
+               
                 foreach (var album in albumList)
                 {
+                    var imageId = _unitOfWork.ImagesAlbums.Find(ia => ia.AlbumsAlbumId == album.AlbumId)
+                        .First()
+                        .ImagesAlbumId;
+
+                    var coverImage = (imageId != null) ? await _unitOfWork.Images.GetAsync((int)imageId) : null;
+                    var coverImageBytes = (coverImage != null) ? await _storageContext.GetAsync(new ImageContainer(), coverImage.ImageSha1) : null;
+                    var coverImageBase64 = (coverImageBytes != null) ? _imageConverter.ImageBytesToBase64(coverImageBytes) : null;
+
                     SingleAlbumViewModel singleAlbumView = new SingleAlbumViewModel
                     {
                         AlbumId = album.AlbumId,
                         ImagesCount = _unitOfWork.ImagesAlbums.Find(image => image.AlbumsAlbumId == album.AlbumId).ToList().Count,
                         AlbumName = album.Name,
-                        AlbumCoverPicture = "no cover yet"
+                        AlbumCoverPicture = coverImageBase64 ?? null
                     };
 
                     albumViewModelList.Add(singleAlbumView);
